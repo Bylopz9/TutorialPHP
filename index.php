a3c4aa764b97d8448011a94e30035439410af6b7
Bylopz9 <lopz_9@outlook.com>
2020-07-22 21:44:56 -0500

    Basico  Declaraciion de variables y declaracion de funciones

--- index.php
+++ index.php
@@ -9,8 +9,30 @@
     
 <?php
 
-print("Hola mundo");
-print("COmo estan")
+print("Hola mundo <br>"); /* Des esta manera se visualiza informacion en el archivo */
+print("Como estan <br>");
+
+/* Tambien se puede mandar informacion con el comando echo */
+echo "Hola mndo 2 <br>";
+
+/* Creacion de variables en PHP */
+
+$Nombre = "Manuel";
+$Apellido = "Lopez";
+
+echo "Soy $Nombre". " $Apellido <br>";  
+
+/* Declaramos funciones cono si de la misma manera que ne java */
+
+function nombrecompleto (){
+    
+    echo "Mi nombre es Manuel Lopez Salas <br>";
+}
+
+echo "separando servicios <br>";
+
+nombrecompleto();
+
 ?>
 
 </body>
